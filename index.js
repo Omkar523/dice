9d033fafcca2d340329b14465815fd778b4ea55e
Omkar523 <67868847+Omkar523@users.noreply.github.com>
2020-07-25 02:42:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ document.querySelector("h1").innerHTML="--"+r1+"--";
 var r1i = "images/dice"+r1+".png";
 document.querySelector(".img1").setAttribute("src",r1i);
 }
+
+document.querySelector("button").addEventListener("click",mouseC)
+function mouseC() {
+
+  
+    document.querySelector(".img1").classList.add("ani");
+    setTimeout(fade,1000) 
+    function fade() 
+        {
+        
+        document.querySelector(".img1").classList.remove("ani")
+        
+        }
+}
